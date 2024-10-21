8a607688bac589c52fa1c6f7737782f3f5c3bf98
Anton Moilanen <anton.a.moilanen@gmail.com>
2024-10-22 00:57:42 +0400

    Optimize encryption and decryption by optimizing the exponentiation by modulo

--- src/modulo_arithmetic.rs
+++ src/modulo_arithmetic.rs
@@ -1,19 +1,18 @@
 use num_bigint::BigInt;
-use num_traits::{FromPrimitive, One, Zero};
+use num_traits::{One, Zero};
 
 pub(crate) fn exponent(number: &BigInt, power: &BigInt, modulo: &BigInt) -> BigInt {
-    let zero = BigInt::zero();
-    let one = BigInt::one();
+    let one = &BigInt::one();
     let mut remaining_power = power.clone();
-    let mut result: BigInt = BigInt::from_u8(1).unwrap();
-    let mut number_to_exponentiate: BigInt = number.clone() % modulo;
+    let mut result: BigInt = BigInt::one();
+    let mut number_to_exponentiate: BigInt = number % modulo;
     while !remaining_power.is_zero() {
-        if &remaining_power & &one == zero {
-            remaining_power = remaining_power / 2;
-            number_to_exponentiate = (&number_to_exponentiate * &number_to_exponentiate) % modulo;
-        } else {
+        if &remaining_power & one == *one {
             remaining_power = remaining_power - 1;
-            result = (result * &number_to_exponentiate) % modulo;
+            result = (&result * &number_to_exponentiate) % modulo;
+        } else {
+            remaining_power >>= 1;
+            number_to_exponentiate = (&number_to_exponentiate * &number_to_exponentiate) % modulo;
         }
     }
     result
